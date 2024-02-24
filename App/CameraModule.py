596bdab8ec7bab1bdb2829ae900a1843bcc36d5b
niklasdiet <niki.di@web.de>
2024-02-24 16:52:16 +0100

    Aktualisiere die Kameraaufnahme-Funktion in CameraModule.py

--- App/CameraModule.py
+++ App/CameraModule.py
@@ -1,10 +1,10 @@
 from time import sleep
-import picamera as picamera
+import picamera
 
 def take_picture():
-    camera = picamera.PiCamera()
-    camera.resolution = (1024, 768)
-    camera.start_preview()
-    sleep(2)
-    camera.capture('images/image.jpg')
-    camera.close()
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.start_preview()
+        sleep(2)
+        camera.capture('images/image.jpg')
+        camera.stop_preview()
